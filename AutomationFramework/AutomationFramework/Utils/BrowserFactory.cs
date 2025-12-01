6df6917e8b73cda6bb8baa11deb242c98fc4ee79
Camilo Fonseca <camilo.fonseca.v@gmail.com>
2025-12-01 16:35:29 -0500

    Configured Browser Factory class

--- AutomationFramework/AutomationFramework/Utils/BrowserFactory.cs
+++ AutomationFramework/AutomationFramework/Utils/BrowserFactory.cs
@@ -1,10 +1,109 @@
-﻿using System;
-using System.Collections.Generic;
-using System.Text;
+﻿using OpenQA.Selenium;
+using OpenQA.Selenium.Chrome;
+using OpenQA.Selenium.Edge;
+using OpenQA.Selenium.Firefox;
+using Microsoft.Extensions.Configuration;
 
 namespace AutomationFramework.Utils
 {
-    internal class BrowserFactory
+    public class BrowserFactory
     {
+        private readonly IWebDriver? _webDriver;
+
+        public BrowserFactory(Browsers browser, string downloadPath = "")
+        {
+            _webDriver = InitBrowser(browser, downloadPath);
+        }
+
+        public IWebDriver InitBrowser(Browsers browser, string downloadPath)
+        {
+            var webDriver = browser switch
+            {
+                Browsers.Chrome => InitChromeDriver(downloadPath),
+                Browsers.Firefox => InitFirefoxDriver(downloadPath),
+                Browsers.Edge => InitEdgeDriver(downloadPath),
+                _ => throw new ArgumentOutOfRangeException(nameof(browser))
+            };
+            return webDriver;
+        }
+        public IWebDriver InitChromeDriver(string downloadPath = "")
+        {
+            ChromeOptions options = new();
+            if (!string.IsNullOrEmpty(downloadPath))
+            {
+                options.AddUserProfilePreference("download.default_directory", downloadPath);
+                options.AddUserProfilePreference("download.prompt_for_download", false);
+                options.AddUserProfilePreference("download.directory_upgrade", true);
+                options.AddUserProfilePreference("safebrowsing.enabled", true);
+            }
+            options.AddArgument("--window-size=1920,1080");
+            options.AddArgument("--headless");
+            var webdriver = new ChromeDriver(options);
+            return webdriver;
+        }
+        public IWebDriver InitFirefoxDriver(string downloadPath = "")
+        {
+            FirefoxOptions options = new();
+            if (!string.IsNullOrEmpty(downloadPath))
+            {
+                FirefoxProfile profile = new();
+                profile.SetPreference("browser.download.dir", downloadPath);
+                profile.SetPreference("browser.download.folderList", 2);
+                profile.SetPreference("browser.download.useDownloadDir", true);
+                profile.SetPreference("browser.helperApps.neverAsk.saveToDisk",
+                        "application/pdf,text/plain,application/octet-stream");
+                profile.SetPreference("pdfjs.disabled", false);
+                profile.SetPreference("browser.helperApps.alwaysAsk.force", false);
+                options.Profile = profile;
+
+            }
+            options.AddArgument("--width=1920");
+            options.AddArgument("--height=1080");
+            options.AddArgument("--headless");
+            var webdriver = new FirefoxDriver(options);
+            return webdriver;
+        }
+        public IWebDriver InitEdgeDriver(string downloadPath = "")
+        {
+            EdgeOptions options = new();
+            if (!string.IsNullOrEmpty(downloadPath))
+            {
+                options.AddUserProfilePreference("download.default_directory", downloadPath);
+                options.AddUserProfilePreference("download.prompt_for_download", false);
+                options.AddUserProfilePreference("download.directory_upgrade", true);
+                options.AddUserProfilePreference("safebrowsing.enabled", true);
+            }
+            options.AddArgument("--window-size=1920,1080");
+            options.AddArgument("--headless");
+            var webDriver = new EdgeDriver(options);
+            return webDriver;
+        }
+        public IWebDriver GetInstanceOf()
+        {
+            if(_webDriver == null)
+            {
+                throw new InvalidOperationException("WebDriver hasn't been initialized");
+            }
+            return _webDriver;
+        }
+        public static string LoadApplication()
+        {
+            var configBuilder = new ConfigurationBuilder()
+                .SetBasePath(Directory.GetCurrentDirectory())
+                .AddJsonFile(Path.Combine("Config","AppSettings.json"));
+            var configuration = configBuilder.Build();
+            string baseUrl = configuration["BaseUrl"] ?? string.Empty;
+            return baseUrl;
+        }
+        public static void CloseAllDrivers(IWebDriver webDriver)
+        {
+            if (webDriver == null)
+            {
+                throw new InvalidOperationException("WebDriver hasn't been initialized");
+            }
+            webDriver.Close();
+            webDriver.Quit();
+            webDriver.Dispose();
+        }
     }
 }
---
+++ AutomationFramework/AutomationFramework/Utils/Browsers.cs
@@ -0,0 +1,13 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace AutomationFramework.Utils
+{
+    public enum Browsers
+    {
+        Chrome,
+        Firefox,
+        Edge
+    }
+}
